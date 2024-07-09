d95eda19d8adbf7b61ad721d1d4538dfc974f952
Leinadium <daniel.sch.guima@gmail.com>
2024-07-09 10:49:52 -0300

    hotfix: allow wrap on section-content

--- homepage/src/lib/Section.svelte
+++ homepage/src/lib/Section.svelte
@@ -66,7 +66,7 @@
         height: 100%;
 
         display: flex;
-        flex-flow: row nowrap;
+        flex-flow: row wrap;
         justify-content: center;
         align-items: start;
         gap: 10%;
